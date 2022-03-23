5f4cce874edd55dd2eef5ff53c04a84c8fc4f398
Figueroa <karfigueroa@FVFZMYC6J1WK.tld>
2022-03-23 10:56:12 -0500

    :)

---
+++ app.js
@@ -0,0 +1,37 @@
+function askName(){
+	var n = prompt(`Hello, what is your name?`)
+	return n
+}
+function greeting (n){       //parameter
+	alert(`Hello ${n}`)
+}
+var n = askName()
+greeting(n)    //argument
+
+
+function askTemp(){
+	var t = prompt (`${n} , insert the degrees you want to convert to`)
+
+	return t 
+}
+askTemp(n)
+
+function askResult(){
+	var result = prompt (`Type c if you want your result to be celsius and f if you want your result to be farenheit`)
+	return result
+}
+
+function ctof(){
+    var celtofar = 
+}
+
+//don't do functions inside if else statements
+
+// if (askResult = c) {
+//     do{
+
+//         var ctof = 
+
+//     }while
+// }
+
