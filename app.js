7c0e9b41d8d4654048f2bea98d60314c4d31182e
Figueroa <karfigueroa@FVFZMYC6J1WK.tld>
2022-03-24 10:56:30 -0500

    unit10commit

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-function askName(){
-	var n = prompt(`Hello, what is your name?`)
-	return n
-}
-function greeting (n){       //parameter
-	alert(`Hello ${n}`)
-}
-var n = askName()
-greeting(n)    //argument
+	function askName(){
+		var n = prompt(`Hello, what is your name?`)
+		return n
+	}
+	
+	askTemp(askName()) //argument
+	
+	function askTemp(n){
+		var t = prompt (`${n}, insert the degrees you want to convert to`)
+		return t
+	}
+
+	function askResult(){
+		var result = prompt (` Type c if you want your result to be celsius and f if you want your result to be farenheit`)
+		return result
+	}
+	var result = (askResult)
+	askResult(result)
 
 
-function askTemp(){
-	var t = prompt (`${n} , insert the degrees you want to convert to`)
+	function cTof(t){
+		var celTemp= (t *9 / 5 + 32)
+		return celTemp
+	}
+	var celTemp = (cTof)
+	cTof(celTemp)
+	
+	  
+	function fToc(t){
+		var farTemp = (t - 32 * 5 / 9)
+		return farTemp
+	}
+	var farTemp = (fToc)
+	fToc(farTemp)
 
-	return t 
-}
-askTemp(n)
+	function end(a, b){
 
-function askResult(){
-	var result = prompt (`Type c if you want your result to be celsius and f if you want your result to be farenheit`)
-	return result
-}
+		if (result=="c"){
+		alert (`${b} in celcius`)
+	  	}else if (result=="f"){
+		alert (`${a} in farenheight`)
+		}
+	}
 
-function ctof(){
-    var celtofar = 
-}
-
-//don't do functions inside if else statements
-
-// if (askResult = c) {
-//     do{
-
-//         var ctof = 
-
-//     }while
-// }
-
+end(fToc())
